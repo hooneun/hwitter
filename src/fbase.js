777c189fc8ac3e33a0c26406162e74495ab06872
김훈 <hooneun@kimhoon-ui-MacBookPro.local>
2020-10-26 22:23:29 +0900

    database settingwrite form add

--- src/fbase.js
+++ src/fbase.js
@@ -1,18 +1,19 @@
-import * as firebase from "firebase/app"
-import "firebase/auth"
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIRE_BASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIRE_BASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIRE_BASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIRE_BASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIRE_BASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIRE_BASE_MESSAGIN_ID,
-    appId: process.env.REACT_APP_FIRE_BASE_APP_ID
-}
+	apiKey: process.env.REACT_APP_FIRE_BASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIRE_BASE_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIRE_BASE_DATABASE_URL,
+	projectId: process.env.REACT_APP_FIRE_BASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIRE_BASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIRE_BASE_MESSAGIN_ID,
+	appId: process.env.REACT_APP_FIRE_BASE_APP_ID
+};
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
-export const firebaseInstance = firebase
-export const authService = firebase.auth()
+export const firebaseInstance = firebase;
+export const authService = firebase.auth();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,28 @@
-import React from "react"
+import React, { useState } from 'react';
 
-const Home = () => <span>Home</span>
+const Home = () => {
+	const [ hwitt, setHwitt ] = useState('');
+	const onSubmit = (event) => {
+		event.preventDefault();
+	};
+	const onChange = (event) => {
+		const { target: { value } } = event;
+		setHwitt(value);
+	};
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<input
+					type="text"
+					value={hwitt}
+					onChange={onChange}
+					placeholder="What's on your mind?"
+					maxLength={12}
+				/>
+				<input type="submit" value="hwitt" />
+			</form>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
